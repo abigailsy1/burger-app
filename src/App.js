cdf81015aa706bc1910eda8d269d82d0201ca863
Abby <abigailsy1@yahoo.com>
2018-09-24 19:44:44 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -70,20 +70,20 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); //classes=['red']
+      assignedClasses.push('classes.red'); //classes=['red']
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); //classes = ['red', 'bold']
+      assignedClasses.push('classes.bold'); //classes = ['red', 'bold']
     }
 
 
     // JSX:
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hello</h1>
-        <p className={classes.join(' ')}>this is tight</p>
+        <p className={assignedClasses.join(' ')}>this is tight</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>
